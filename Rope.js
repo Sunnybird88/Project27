8478670003af1c27199ad08964de5f1ccd469cbd
Amitesh Krishna <anantk124@gmail.com>
2020-09-30 13:30:55 +0530

    Updated index.html , Rope.js,sketch.js

--- Rope.js
+++ Rope.js
@@ -1,19 +1,19 @@
-class Chain{
+class Rope{
 
-    constructor(objA,objB) {
+    constructor(objA,objB,offX,offY) {
         
         var options = 
         {
             bodyA:objA.body,
-            bodyB:objB.body,
+            bodyB:objB.body
+            pointB:{x:offX,y:offY},
             stiffness:0.05,
             length:10
 
         }
 
         this.posA = objA.body.position;
-        this.posB = objB.body.position;
-
+        this.posB = ;
 
         this.connection = Constraint.create(options);
         World.add(world,this.connection)
--- sketch.js
+++ sketch.js
@@ -1,23 +1,13 @@
-
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
-function preload()
-{
-	
-}
-
 function setup() {
 	createCanvas(800, 700);
 
-
 	engine = Engine.create();
 	world = engine.world;
-
-	//Create the Bodies Here.
-
 
 	Engine.run(engine);
   
@@ -25,11 +15,9 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+
+  background(230,230,230);
+
 }
 
 
